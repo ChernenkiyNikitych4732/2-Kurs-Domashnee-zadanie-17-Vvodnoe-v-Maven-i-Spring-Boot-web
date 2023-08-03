f6bf7d955af0b4d4266a2cc0fba4ae007f705016
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2023-08-03 16:26:23 +0100

    2 Курс ДЗ №17 Вводное в Maven и Spring Boot + web

---
+++ src/main/java/sky/pro/homeworkmavenspringboot/Kurs2DomashneeZadanie17VvodnoeVMavenISpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.homeworkmavenspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs2DomashneeZadanie17VvodnoeVMavenISpringBootWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs2DomashneeZadanie17VvodnoeVMavenISpringBootWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/homeworkmavenspringboot/ApplicationTests.java
@@ -0,0 +1,12 @@
+package sky.pro.homeworkmavenspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs2DomashneeZadanie17VvodnoeVMavenISpringBootWebApplicationTests  {
+
+    @Test
+    void contextLoads() {
+    }
+}

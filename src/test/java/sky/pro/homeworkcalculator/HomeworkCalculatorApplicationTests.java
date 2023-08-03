9a8f4d30a0678ee48d4e11a5af8252e9323327dd
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2023-08-03 16:38:06 +0100

    2 Курс ДЗ №17 Вводное в Maven и Spring Boot + web

---
+++ src/test/java/sky/pro/homeworkcalculator/HomeworkCalculatorApplicationTests.java
@@ -0,0 +1,12 @@
+package sky.pro.homeworkcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
